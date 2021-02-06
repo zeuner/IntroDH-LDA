faf4b4d09d575b17e2236142e2921c5b8d555c31
Agoston Volcz <agoston.volcz@gmail.com>
2021-02-06 21:21:15 +0100

    Downloading and converting implemented, still needs cleaning

--- scrape-cz.R
+++ scrape-cz.R
@@ -5,6 +5,8 @@
 
 library(tidyverse)
 library(tidytext)
+library(jsonlite)
+library(pdftools)
 source("scrape.R")
 
 ## -------------------------------------------------------------------
@@ -37,4 +39,78 @@ download_results <-
                print(return_value)
                return(return_value)})
                                                     
-writeLines(unlist(download_results),"./results/download_results-cz.txt")                                  
+writeLines(unlist(download_results),"./results/cz-download_results.txt")                                  
+
+
+## ===================================================================
+## Processing pdf files
+
+newline <- function (x,y) {
+    if (!y) {
+        paste(x,"\n",sep="")}
+    else x}
+
+insert_newlines <- function (t) {
+    words <- t$text
+    spaces <- t$space
+    mapply(newline, words, spaces)}
+
+unhyphenate <- function (data) {
+    trimws(data) %>%
+        paste(collapse="") %>% 
+        str_replace_all(pattern="-\n ?([^0-9 ]+) ",replacement="\\1\n") %>%
+        str_split("\n") %>%
+        sapply(trimws)}
+
+remove_page_numbering <- function (page) {
+                pageno_pos <- last(page$y)
+                x <- filter(page, y != pageno_pos)  # removing page numberings
+                if (length(page$text) == 0) {
+                    NULL
+                } else page }
+
+pdf_to_txt <- function(infile, outfile, nth, total) {
+    if (! file.exists(outfile)){
+        pdf_data(infile) %>%
+            lapply(remove_page_numbering) %>% # removing empty pages
+            .[sapply(.,Negate(is.null))] %>%
+            lapply(insert_newlines) %>%
+            unlist %>%
+            paste(collapse=" ") %>%
+            unhyphenate %>%
+            trimws %>%
+            writeLines(outfile)
+        return(sprintf("[%3d/%3d] Converted %s -> %s",
+                       nth,total,infile,outfile))}
+    else { return(sprintf("[%3d/%3d] %s already present.",nth,total,outfile))}}
+
+
+convert_pdf <- function(nth,pdfs) {
+    infile <- pdfs[nth]
+    outfile <- str_replace_all(infile,"pdf","txt")
+    total <- length(pdfs)
+    return(pdf_to_txt(infile,outfile,nth,total))
+    }
+
+pdfs <- Sys.glob("./pdfs/cz-*.pdf")
+    
+convert_results <- 
+    future.apply::future_lapply(future.seed=TRUE,1:length(pdfs),
+                                function (x) {
+                                    return_value <- convert_pdf(x,pdfs)
+                                    print(return_value)
+                                    return(return_value)})
+
+writeLines(unlist(convert_results),"./results/cz-convert_results.txt")
+
+id <- Sys.glob("./txts/cz-*.txt")
+
+data <- sapply(id, read_file)
+
+exported <- data.frame(id, data)
+
+json <- toJSON(exported)
+
+write(json, file="czechia.json")
+
+
