2568b0d1ac080d58dff97c0ac0fb1b358ae95c33
Isidor Zeuner <dh2021@quidecco.de>
2021-02-28 13:54:11 +0100

    split out country-specific LDA settings into a data structure

--- lda-generic.R
+++ lda-generic.R
@@ -9,51 +9,55 @@ library(jsonlite)
 library(hunspell)
 library(SnowballC)
 library(textmineR)
+library(hash)
 
 cpus_lda <- parallel::detectCores()
 
-dtm_cache_file <- "hungary-dtm.rds"
+country_settings <- hash()
 
-if (file.exists(dtm_cache_file)) {
-    dtm <- readRDS(dtm_cache_file)
-} else {
-    frame <- fromJSON(read_file("hungary.json"))
-
-    stemming_function <- function (x) unlist(hunspell_stem(x,"hu_HU"))
-
-    dtm <- CreateDtm(
-        doc_vec = frame$data,
-        doc_names = frame$id,
-        ngram_window = c(1, 1),
-        stopword_vec = c(stopwords::stopwords("hu")),
-        lower = TRUE,
-        remove_punctuation = TRUE,
-        remove_numbers = TRUE,
-        verbose = FALSE,
-        stem_lemma_function = stemming_function,
-        cpus = cpus_lda
-    )
-
-    saveRDS(dtm, file = dtm_cache_file)
-}
-## arbitrary seeding for reproducible output
-set.seed(12833)
-
-model <- FitLdaModel(
-    dtm = dtm,
-    k = 20,
-    iterations = 500,
-    burnin = 180,
-    alpha = 0.1,
-    beta = 0.05,
-    optimize_alpha = TRUE,
-    calc_likelihood = TRUE,
-    calc_coherence = TRUE,
-    calc_r2 = TRUE,
-    cpus = cpus_lda
+country_settings[["hungary"]] <- list(
+    locale = "hu_HU",
+    language = "hu"
 )
 
-## sample output of top terms for each topic
-model$top_terms <- GetTopTerms(phi = model$phi, M = 5)
-
-head(t(model$top_terms))
+analyze_country <- function (country_name) {
+    settings <- country_settings[[country_name]]
+    dtm_cache_file <- paste0(country_name, "-dtm.rds")
+    if (file.exists(dtm_cache_file)) {
+        dtm <- readRDS(dtm_cache_file)
+    } else {
+        frame <- fromJSON(read_file(paste0(country_name, ".json")))
+        stemming_function <- function (x) {
+            unlist(hunspell_stem(x, settings$locale))
+        }
+        dtm <- CreateDtm(
+            doc_vec = frame$data,
+            doc_names = frame$id,
+            ngram_window = c(1, 1),
+            stopword_vec = c(stopwords::stopwords(settings$language)),
+            lower = TRUE,
+            remove_punctuation = TRUE,
+            remove_numbers = TRUE,
+            verbose = FALSE,
+            stem_lemma_function = stemming_function,
+            cpus = cpus_lda
+        )
+        saveRDS(dtm, file = dtm_cache_file)
+    }
+## arbitrary seeding for reproducible output
+    set.seed(12833)
+    model <- FitLdaModel(
+        dtm = dtm,
+        k = 20,
+        iterations = 500,
+        burnin = 180,
+        alpha = 0.1,
+        beta = 0.05,
+        optimize_alpha = TRUE,
+        calc_likelihood = TRUE,
+        calc_coherence = TRUE,
+        calc_r2 = TRUE,
+        cpus = cpus_lda
+    )
+    return (model)
+}
---
+++ lda-hu.R
@@ -0,0 +1,15 @@
+## Description: Extract topics from JSON-supplied debates
+## Author: Isidor Zeuner
+## dh2021@quidecco.de
+## For license see: https://mit-license.org
+## -------------------------------------------------------------------
+
+library(textmineR)
+source("lda-generic.R")
+
+model <- analyze_country("hungary")
+
+## sample output of top terms for each topic
+model$top_terms <- GetTopTerms(phi = model$phi, M = 5)
+
+head(t(model$top_terms))
